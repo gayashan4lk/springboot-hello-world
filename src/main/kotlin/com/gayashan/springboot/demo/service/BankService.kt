addfa448567fab86dac6b2f33813258986192818
gayashan4lk <gayashan4lk@gmail.com>
2024-03-10 15:55:49 +0530

    add initial test for BankService

--- src/main/kotlin/com/gayashan/springboot/demo/service/BankService.kt
+++ src/main/kotlin/com/gayashan/springboot/demo/service/BankService.kt
@@ -1,8 +1,12 @@
 package com.gayashan.springboot.demo.service
 
 import com.gayashan.springboot.demo.datasource.BankDataSource
+import com.gayashan.springboot.demo.model.Bank
 import org.springframework.stereotype.Service
 
 @Service
 class BankService(private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> {
+        return dataSource.retrieveBanks();
+    }
 }
--- src/test/kotlin/com/gayashan/springboot/demo/service/BankServiceTest.kt
+++ src/test/kotlin/com/gayashan/springboot/demo/service/BankServiceTest.kt
@@ -1,20 +1,26 @@
 package com.gayashan.springboot.demo.service
 
 import com.gayashan.springboot.demo.datasource.BankDataSource
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BankServiceTest{
-//    private val dataSource : BankDataSource = null
-//    private val bankService = BankService()
+    private val dataSource : BankDataSource = mockk()
+    private val bankService = BankService(dataSource)
 
     @Test
     fun `should call data source to retrieve banks`() {
         // give
+        every { dataSource.retrieveBanks() } returns emptyList()
 
         // when
-//        val banks = bankService
+        val banks = bankService.getBanks()
 
         // then
+        verify(exactly = 1) { dataSource.retrieveBanks() }
     }
 }

eb8f26e2599ac94717c46142382cfbebd19640a9
gayashan4lk <gayashan4lk@gmail.com>
2024-03-10 19:37:59 +0530

    GET Banks endpoint

--- src/main/kotlin/com/gayashan/springboot/demo/controller/BankController.kt
+++ src/main/kotlin/com/gayashan/springboot/demo/controller/BankController.kt
@@ -1,7 +1,15 @@
 package com.gayashan.springboot.demo.controller
 
+import com.gayashan.springboot.demo.model.Bank
+import com.gayashan.springboot.demo.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BankController {
+@RequestMapping("api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
 }
--- src/test/kotlin/com/gayashan/springboot/demo/controller/BankControllerTest.kt
+++ src/test/kotlin/com/gayashan/springboot/demo/controller/BankControllerTest.kt
@@ -1,11 +1,11 @@
 package com.gayashan.springboot.demo.controller
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,10 +16,9 @@ class BankControllerTest {
 
     @Test
     fun `should return all banks`() {
-        // given
-
-        // when
-
-        // then
+        // when/then
+        mockMvc.get("/api/banks")
+            .andDo { print() }
+            .andExpect { status { isOk() } }
     }
 }

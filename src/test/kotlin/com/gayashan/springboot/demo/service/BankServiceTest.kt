d9d376f5fb46c2b4bffb7432f5b632c940a4a097
gayashan4lk <gayashan4lk@gmail.com>
2024-03-10 15:56:27 +0530

    refactor BankServiceTest

--- src/test/kotlin/com/gayashan/springboot/demo/service/BankServiceTest.kt
+++ src/test/kotlin/com/gayashan/springboot/demo/service/BankServiceTest.kt
@@ -9,13 +9,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BankServiceTest{
-    private val dataSource : BankDataSource = mockk()
+    private val dataSource : BankDataSource = mockk(relaxed = true)
     private val bankService = BankService(dataSource)
 
     @Test
     fun `should call data source to retrieve banks`() {
-        // give
-        every { dataSource.retrieveBanks() } returns emptyList()
+        // given
+//        every { dataSource.retrieveBanks() } returns emptyList()
 
         // when
         val banks = bankService.getBanks()

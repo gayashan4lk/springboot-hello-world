6f74d4537d5a020786549b37f896a0c706c4de0a
gayashan4lk <gayashan4lk@gmail.com>
2024-03-12 06:22:23 +0530

    update hello-world endpoint

--- src/main/kotlin/com/gayashan/springboot/demo/HelloWorldController.kt
+++ src/main/kotlin/com/gayashan/springboot/demo/HelloWorldController.kt
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api/hello")
+@RequestMapping("api/hello-world")
 class HelloWorldController {
     @GetMapping
-    fun helloWorld(): String = "Hello, Spring Boot! This is a GET endpoint."
+    fun helloWorld(): String = "Hello, Spring Boot!"
 }

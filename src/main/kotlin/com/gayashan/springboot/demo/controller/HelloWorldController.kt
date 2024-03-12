dd5a12c3cc8e1c5243f4f15816a5f52dc3e1d188
gayashan4lk <gayashan4lk@gmail.com>
2024-03-12 06:24:04 +0530

    restructure files

--- src/main/kotlin/com/gayashan/springboot/demo/controller/HelloWorldController.kt
+++ src/main/kotlin/com/gayashan/springboot/demo/controller/HelloWorldController.kt
@@ -1,4 +1,4 @@
-package com.gayashan.springboot.demo
+package com.gayashan.springboot.demo.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
